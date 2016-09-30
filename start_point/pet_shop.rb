edc7dfad0ebebc42ec7888bf6e73a6bba18056cb
Clacson <cookiellacson@gmail.com>
2016-09-30 14:44:30 +0100

    Third test passed.

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,3 +8,10 @@ def total_cash(shop)
   return @pet_shop[:admin][:total_cash]
 end
 
+def add_or_remove_cash( shop, added_cash )
+  return @pet_shop[:admin][:total_cash] += added_cash
+end
+
+# def total_cash(shop)
+#   return @pet_shop[:admin][:total_cash]
+# end

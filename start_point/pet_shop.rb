a5e07dad81d0a651caea398ee737079f2cab2967
Clacson <cookiellacson@gmail.com>
2016-09-30 15:05:36 +0100

    Seventh test passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -20,6 +20,15 @@ def add_or_remove_cash( shop, added_cash )
   return @pet_shop[:admin][:total_cash] += added_cash
 end
 
-# def pets_sold( shop )
-#   return @pet_shop[:admin][:pets_sold]
-# end
+def pets_sold( shop )
+  return @pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold( shop, pets )
+  pets_sold = @pet_shop[:admin][:pets_sold] += pets
+  return pets_sold
+end
+
+def stock_count ( shop )
+  return @pet_shop[:pets].count
+end
